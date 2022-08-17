533d97f4436ad4f84152eaa150595992f088f960
Pete Fowler <fowler.pete@gmail.com>
2022-08-17 17:06:37 -0600

    Add getMenu, addItems

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here...
+let localData;
+const menu = document.querySelector('#menu-items');
+
+const addItems = (data) => {
+  data.forEach(el => {
+    const span = document.createElement('span');
+    span.textContent = el.name;
+    menu.append(span);
+  });
+}
+
+const getMenu = () => {
+  fetch(`http://localhost:3000/menu`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    addItems(data);
+    localData = data;
+  })
+}
+
+const init = (() => {
+  getMenu();
+})();
