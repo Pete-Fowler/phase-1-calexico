27180e93d138f229d6166d4ec7c89d9852bd9126
Pete Fowler <fowler.pete@gmail.com>
2022-08-17 17:17:17 -0600

    Add addDetail

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-let localData;
+let menuObj;
+let clicked;
 const menu = document.querySelector('#menu-items');
+const image = document.querySelector('#dish-image');
+const title = document.querySelector('#dish-name');
+const desc = document.querySelector('#dish-description');
+const price = document.querySelector('#dish-price');
+
+const cart = () => {
+  
+}
 
 const addItems = (data) => {
-  data.forEach(el => {
+  data.forEach(dish => {
     const span = document.createElement('span');
-    span.textContent = el.name;
+    span.textContent = dish.name;
     menu.append(span);
+    span.addEventListener('click', () => addDetail(dish))
   });
+}
+
+const addDetail = (item) => {
+  clicked = item;
+  image.src = item.image;
+  title.textContent = item.name;
+  desc.textContent = item.description;
+  price.textContent = item.price;
 }
 
 const getMenu = () => {
   fetch(`http://localhost:3000/menu`)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
     addItems(data);
-    localData = data;
+    addDetail(data[0]);
+    menuObj = data;
   })
 }
 
