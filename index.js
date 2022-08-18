3bd63d1227eee02b2086003c66c163f76590a7ba
Pete Fowler <fowler.pete@gmail.com>
2022-08-17 20:56:51 -0600

    Add patch

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const image = document.querySelector('#dish-image');
 const title = document.querySelector('#dish-name');
 const desc = document.querySelector('#dish-description');
 const price = document.querySelector('#dish-price');
+const form = document.querySelector('#cart-form');
+const input = document.querySelector('#cart-amount');
+const count = document.querySelector('#number-in-cart');
 
-const cart = () => {
-  
+const cart = (e) => {
+  e.preventDefault();
+  clicked.number_in_bag += parseInt(input.value);
+  count.textContent = clicked.number_in_bag;
+  const config = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept:": "applicaton/json",
+    },
+    body: JSON.stringify({
+      "number_in_bag": clicked.number_in_bag,
+    }),
+  };
+  fetch(`http://localhost:3000/menu/${clicked.id}`, config)
+  .then(res => res.json())
+  .then(data => console.log(data))
+  // .catch(err => console.log(err.message));
 }
 
 const addItems = (data) => {
@@ -25,6 +44,8 @@ const addDetail = (item) => {
   title.textContent = item.name;
   desc.textContent = item.description;
   price.textContent = item.price;
+  count.textContent = item.number_in_bag;
+  input.value = '';
 }
 
 const getMenu = () => {
@@ -39,4 +60,5 @@ const getMenu = () => {
 
 const init = (() => {
   getMenu();
+  form.addEventListener('submit', cart);
 })();
